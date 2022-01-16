5977cd03dfe27f583a8f5f5343b9d5056064d00c
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-01-16 22:34:58 +0530

    temp fix : TypeError: MiniCssExtractPlugin is not a constructor
    - this is temporary solution
    - refer https://github.com/webpack-contrib/mini-css-extract-plugin/issues/896

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin").default;
 const CopyPlugin = require('copy-webpack-plugin');
 
 
 module.exports = {
-    mode: 'production',
-    plugins: [
-        new MiniCssExtractPlugin(
-            {
-                filename: "[name].css",
-            }
-        ),
-        new CopyPlugin({
-            patterns: [
-                { from: "*.html", context: path.resolve(__dirname, 'src') },
+  mode: 'production',
+  plugins: [
+    new MiniCssExtractPlugin(
+      {
+        filename: "[name].css",
+      }
+    ),
+    new CopyPlugin({
+      patterns: [
+        { from: "*.html", context: path.resolve(__dirname, 'src') },
         { from: "assets", to: "./assets", context: path.resolve(__dirname, 'src'), noErrorOnMissing: true, },
-            ],
-        }),
-    ],
+      ],
+    }),
+  ],
   entry: path.resolve(__dirname, 'src', 'scss', 'style.scss'),
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                      },
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'sass-loader',
         ],
-    },
+      },
+    ],
+  },
 };
