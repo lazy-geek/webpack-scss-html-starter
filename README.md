201f43c31bd8dfdc9c443fd9bb0104b2f69842a3
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-01-16 21:19:06 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Webpack + SCSS + Html Starter
+Webpack configured for compiling and bundling SCSS with Html (without Javascript Code)
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+
+
+module.exports = {
+    mode: 'production',
+    plugins: [
+        new MiniCssExtractPlugin(
+            {
+                filename: "[name].css",
+            }
+        ),
+        new CopyPlugin({
+            patterns: [
+                { from: "*.html", context: path.resolve(__dirname, 'src') },
+                { from: "assets", to: "./assets", context: path.resolve(__dirname, 'src') },
+            ],
+        }),
+    ],
+    entry: path.resolve(__dirname, 'src','scss','style.scss'),
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                      },
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+};
